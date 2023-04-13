23ad4226a6970d11d6a3f71826abb0158792f016
Pog Du Hog <gitcommits@pogduhog.com>
2023-04-13 13:22:35 +0000

    add bootstrap-k8s.sh

---
+++ bootrap-k8s.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+#set -e
+
+LF="$(printf '\nq')"; LF=${LF%q} 
+TMP=$(mktemp -d)
+
+clean_up() {
+	rm -r $TMP
+	exit "$1"
+}
+
+trap 'clean_up ${?}' EXIT
+
+msg() {
+	echo ">>> $1"
+}
+
+GIT_USER="pogduhog"
+GIT_HOST="github.com"
+GIT_ORG="pogduhog"
+GIT_REPO="bootstrap-argocd"
+GIT_REPO="homelab-k8s"
+SUDO='doas'
+
+install_metallb_manifest() {
+	msg "Installing Metallb..."
+	mkdir -p apps/metallb
+	cat <<-EOF > apps/metallb/addresses.yaml
+		apiVersion: metallb.io/v1beta1
+		kind: IPAddressPool
+		metadata:
+		  name: first-pool
+		  namespace: metallb-system
+		spec:
+		  addresses:
+		  - 10.0.0.50-10.0.0.70
+		---
+		apiVersion: metallb.io/v1beta1
+		kind: L2Advertisement
+		metadata:
+		  name: metallb-l2
+		  namespace: metallb-system
+		spec:
+		  ipAddressPools:
+		  - first-pool
+	EOF
+	doas helm repo add metallb https://metallb.github.io/metallb
+	doas helm install metallb metallb/metallb --create-namespace --namespace metallb-system 
+	doas kubectl apply -f apps/metallb/addresses.yaml
+}
+
+install_metallb() {
+	echo "Installing Metallb..."
+	mkdir -p apps/metallb/base
+	cfg_metallb_kustomization > apps/metallb/base/kustomization.yaml
+	cfg_metallb_l2_addresses > apps/metallb/base/l2_addresses.yaml
+	cfg_metallb_l2_advertise > apps/metallb/base/l2_advertise.yaml
+	doas kubectl apply -k apps/metallb/base/
+}
+
+cfg_metallb_kustomization() {
+	# https://metallb.universe.tf/installation/#installation-with-kustomize
+		#- github.com/metallb/metallb/config/native?ref=v0.13.9
+	cat <<-EOF 
+		apiVersion: kustomize.config.k8s.io/v1beta1
+		kind: Kustomization
+		
+		namespace: metallb-system
+
+		resources:
+		  - github.com/metallb/metallb/config/crd?ref=v0.13.9
+		  - l2_addresses.yaml
+		  - l2_advertise.yaml
+	EOF
+}
+
+install_flux_cli() {
+	echo "Installing flux cli"
+	curl -s https://fluxcd.io/install.sh | doas bash
+}
+
+SSH_KEY_PRIV="../id_ed25519"
+install_flux() {
+	flux bootstrap git --help
+	doas flux bootstrap git \
+		--url=ssh://git@$GIT_HOST/$GIT_ORG/$GIT_REPO \
+		--private-key-file $SSH_KEY_PRIV \ 
+		--branch=main \
+		--path=clusters/production
+}
+
+case "$1" in 
+	install-flux-cli)
+		install_flux_cli
+		;;
+	install-flux)
+		install_flux
+		;;
+	*)	echo "Unknown option $1"
+		exit 1
+		;;
+esac
